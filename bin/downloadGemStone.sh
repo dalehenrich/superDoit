18714d09f676bcd01da7c35014f478fb5de01d76
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-04-11 13:02:56 -0700

    I think I've finally got it

--- bin/downloadGemStone.sh
+++ bin/downloadGemStone.sh
@@ -8,13 +8,13 @@ if [ "$PLATFORM" = "macos-10.15" ] ; then
   attach_result=`hdiutil attach -plist $dmgfile`
   attach_device=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='dev-entry']/following-sibling::string[1]/text()" 2>/dev/null`
   attach_path=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='mount-point']/following-sibling::string[1]/text()" 2>/dev/null`
-  ln -s "${attach_path}/${vers}/${name}" .
+  cp -R "${attach_path}/${gsvers}/${name}" .
  	hdiutil detach ${attach_device}
 else
   name=GemStone64Bit${vers}-x86_64.Linux
   zipfile=${name}.zip
   curl -O -s -S https://downloads.gemtalksystems.com/pub/GemStone64/${vers}/$zipfile
   unzip -q $zipfile
-  ln -s $name product
 fi 
+ln -s $name product
 
