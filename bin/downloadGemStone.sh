ae383ba99044b91acc80a5abfadab6c474c86bb8
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-04-11 12:52:12 -0700

    reformat and clean up downlodGemStone.sh

--- bin/downloadGemStone.sh
+++ bin/downloadGemStone.sh
@@ -1,24 +1,20 @@
 #! /usr/bin/env bash
 
 set -x
-            if [ "$PLATFORM" = "macos-10.15" ] ; then
-              name=GemStone64Bit${vers}-i386.Darwin
-              dmgfile=${name}.dmg
-              curl -O -s -S https://downloads.gemtalksystems.com/pub/GemStone64/${vers}/$dmgfile
-              attach_result=`hdiutil attach -plist $dmgfile`
-      	      attach_device=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='dev-entry']/following-sibling::string[1]/text()" 2>/dev/null`
-      	      attach_path=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='mount-point']/following-sibling::string[1]/text()" 2>/dev/null`
-      	      cp -R "${attach_path}/${gsvers}" .
-      	hdiutil detach ${attach_device}
-              unzip -q $zipfile
-            else
-              export name=GemStone64Bit${vers}-x86_64.Linux
-              zipfile=${name}.zip
-              curl -O -s -S https://downloads.gemtalksystems.com/pub/GemStone64/${vers}/$zipfile
-							ls -l *
-              unzip -q $zipfile
-            fi 
-            ls -l *
-            echo "-->${name}<--"
-            ln -s $name product
+if [ "$PLATFORM" = "macos-10.15" ] ; then
+	name=GemStone64Bit${vers}-i386.Darwin
+  dmgfile=${name}.dmg
+  curl -O -s -S https://downloads.gemtalksystems.com/pub/GemStone64/${vers}/$dmgfile
+  attach_result=`hdiutil attach -plist $dmgfile`
+  attach_device=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='dev-entry']/following-sibling::string[1]/text()" 2>/dev/null`
+  attach_path=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='mount-point']/following-sibling::string[1]/text()" 2>/dev/null`
+  ln -s "${attach_path}/${gsvers}" .
+ 	hdiutil detach ${attach_device}
+else
+  name=GemStone64Bit${vers}-x86_64.Linux
+  zipfile=${name}.zip
+  curl -O -s -S https://downloads.gemtalksystems.com/pub/GemStone64/${vers}/$zipfile
+  unzip -q $zipfile
+  ln -s $name product
+fi 
 
