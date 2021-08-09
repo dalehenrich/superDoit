dalehenrich/superDoit

commit 6b3f594d0514b9124998568a70608f427328737d

added support for 3.4.x .stone scripts ... need to work on topaz exit for 3.4.0 (first version without topaz -S option

bin/generateBootstrapGsFiles.solo
@@ -59,10 +59,14 @@ generateVersionMap
 	| dict orderedDict |
 	dict := Dictionary new.
 	"this list should be updated whenever support for a new gemstone version is added"
+	#('3.4.0' '3.4.1' '3.4.2' '3.4.3' '3.4.4' '3.4.5') 
+		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_34x' ].
 	#('3.5.0' '3.5.1' '3.5.2' '3.5.3' '3.5.4' '3.5.5' '3.5.6') 
 		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_35x' ].
-	#('3.6.0' '3.6.1') do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_36x' ].
-	#('3.7.0') do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_37x' ].
+	#('3.6.0' '3.6.1') 
+		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_36x' ].
+	#('3.7.0') 
+		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_37x' ].
 	orderedDict := GsTonelOrderedDictionary new.
 	dict keys sort do: [:key | orderedDict at: key put: (dict at: key) ].
 	self supportedGemStoneVersionMapReference 

bin/superdoit_stone
@@ -96,7 +96,8 @@ if [ "$useBootstrapSuperDoit_file" = "true" ]; then
 	# Rowan extent NOT NEEDED in stone ... bootstrap superDoit using .gs files
 	vers=`awk 'NR==2{print $1; exit}' $GEMSTONE/version.txt`
 	case $vers in
-		3.1.0.6|3.4.0|3.5.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_35x.gs" ;;
+		3.1.0.6|3.4.0) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_34x.gs" ;;
+		3.5.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_35x.gs" ;;
 		3.6.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_36x.gs" ;;
 		3.7.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_37x.gs" ;;
 		*) echo "unsupported gemstone version $vers"; exit 1 ;;
@@ -114,9 +115,8 @@ case $vers in
 			-q \
 			-S $superdoit_tpz -- $scriptFile -- $scriptArgs ;;
 	*)
+		export superdoit_tpz
 		$GEMSTONE/bin/topaz $defaultTopazArgs $topazArgs \
-			-I "$SUPERDOIT_STONE_DIR/.topazini" -- $scriptFile -- $scriptArgs << EOF
-			input $superdoit_tpz
-EOF
+			-I "$gemstoneDir/topazini" -- $scriptFile -- $scriptArgs
 		;;
 esac

gemstone/bootstrap/bootstrapSuperDoit_34x.gs
@@ -1,5 +1,5 @@
 ! superDoit fileout
-!	2021-08-09T12:34:23.344434-07:00
+!	2021-08-09T12:34:23.284653-07:00
 
 ! Class Declarations
 ! Generated file, do not Edit
@@ -1607,16 +1607,20 @@ _specsDict
 
 !		Instance methods for 'SuperDoitCommand'
 
-category: '*superdoit-core36x'
+category: '*superdoit-core31-4'
 method: SuperDoitCommand
 compileMethod: methodSource for: aBehavior
-	aBehavior compileMethod: methodSource
+	aBehavior 
+		compileMethod: methodSource 
+		dictionaries: GsCurrentSession currentSession symbolList 
+		category: 'superdoit' 
+		environmentId: 0
 %
 
-category: '*superdoit-core36x'
+category: '*superdoit-core31-5'
 method: SuperDoitCommand
 symbolListExpressionString
-	^ 'GsCurrentSession currentSession transientSymbolList'
+	^ 'GsCurrentSession currentSession symbolList'
 %
 
 ! Class extensions for 'SuperDoitCommandParser'
@@ -1677,7 +1681,7 @@ scriptPath: aFilePath
 	scriptPath := aFilePath
 %
 
-category: '*superdoit-core36x'
+category: '*superdoit-core31-5'
 method: SuperDoitCommandParser
 systemDictionary
 	"install the script class in a symbol dictionary in the transient symbol list ... 
@@ -1687,8 +1691,7 @@ systemDictionary
 		ifNil: [ 
 			| sess symbolList name |
 			sess := GsCurrentSession currentSession.
-			sess _transientSymbolList ifNil: [ System refreshTransientSymbolList ].
-			symbolList := sess transientSymbolList.
+			symbolList := sess symbolList.	"transient symbol list"
 			name := self class transientSymbolDictionaryName.
 			^ systemDictionary := (symbolList resolveSymbol: name)
 				ifNotNil: [ :assoc | assoc value ]
@@ -1713,37 +1716,34 @@ dirname
 	self _splitName: self scriptPath to: [ :parentPath :basename | ^ parentPath ]
 %
 
-category: '*superdoit-core36x'
+category: '*superdoit-core31-5'
 method: SuperDoitExecution
 doit
 	"standard option handling ... for customization override in script"
 
-	[ 
+	"no special error handling ... with topaz -S option, exiting topaz with an
+		error message is not feasible, so we'll just stay in permanent debug mode"
+
 	self getAndVerifyOptions == self noResult
 		ifTrue: [ ^ self noResult ].
-	^ self theDoit ]
-		on: Error
-		do: [ :ex | 
-			((self respondsTo: #'debug') and: [ self debug ])
-				ifTrue: [ ex pass ].
-			self exit: ex messageText withStatus: 1	"does not return" ]
+	^ self theDoit
 %
 
-category: '*superdoit-core36x'
+category: '*superdoit-core31-5'
 method: SuperDoitExecution
 exit: message withStatus: statusInteger
-	"Does not return and terminates the os process with exit status <statusInteger>"
+	"exiting image with an exit status not supported until 3.6.0"
 
 	self logErrorMessage: message.
-	ExitClientError signal: 'explicit process exit' status: statusInteger
+	System logout
 %
 
-category: '*superdoit-core36x'
+category: '*superdoit-core31-5'
 method: SuperDoitExecution
 exitWithStatus: statusInteger
-	"Does not return and terminates the os process with exit status <statusInteger>"
+	"exiting image with an exit status not supported until 3.6.0"
 
-	ExitClientError signal: 'explicit process exit' status: statusInteger
+	System logout
 %
 
 category: '*superdoit-stone-core'

gemstone/bootstrap/bootstrapSuperDoit_35x.gs
@@ -1,5 +1,5 @@
 ! superDoit fileout
-!	2021-08-09T09:46:21.688904-07:00
+!	2021-08-09T12:34:23.208774-07:00
 
 ! Class Declarations
 ! Generated file, do not Edit
@@ -610,13 +610,16 @@ executeAgainst: aCommandParser
 		do: [ :option | 
 			| longName |
 			longName := option longName.
-			aCommandParser superDoitExecutionClass
+			self
 				compileMethod:
-					longName , '  ^ (self optionsDict at: ' , longName printString , ') value'.
-			aCommandParser superDoitExecutionClass
+					longName , '  ^ (self optionsDict at: ' , longName printString , ') value'
+				for: aCommandParser superDoitExecutionClass.
+
+			self
 				compileMethod:
 					longName , ': optionValue (self optionsDict at:' , longName printString
-						, ') value: optionValue' ]
+						, ') value: optionValue'
+				for: aCommandParser superDoitExecutionClass ]
 %
 
 category: 'accessing'
@@ -647,10 +650,11 @@ category: 'execution'
 method: SuperDoitDoitCommand
 executeAgainst: aCommandParser
 	| instance |
-	aCommandParser superDoitExecutionClass
+	self
 		compileMethod:
 			'theDoit ^ ' , self chunk printString , ' evaluateInContext: self symbolList: '
-				, self symbolListExpressionString.
+				, self symbolListExpressionString
+		for: aCommandParser superDoitExecutionClass.
 	instance := aCommandParser superDoitExecutionClass new.
 	instance scriptPath: aCommandParser scriptPath.
 	instance scriptArgs: aCommandParser scriptArgs.
@@ -707,7 +711,7 @@ executeAgainst: aCommandParser
 			beh := self isMeta
 				ifTrue: [ class class ]
 				ifFalse: [ class ].
-			beh compileMethod: self chunk ]
+			self compileMethod: self chunk for: beh ]
 		ifNil: [ self error: 'No class ' , className printString , ' found' ]
 %
 
@@ -779,7 +783,7 @@ preClassCreationExecuteAgainst: aCommandParser
 category: 'execution'
 method: SuperDoitMethodCommand
 executeAgainst: aCommandParser
-	aCommandParser superDoitExecutionClass compileMethod: self chunk
+	self compileMethod: self chunk for: aCommandParser superDoitExecutionClass
 %
 
 ! Class implementation for 'SuperDoitProjectsHomeCommand'
@@ -1602,6 +1606,12 @@ _specsDict
 ! Class extensions for 'SuperDoitCommand'
 
 !		Instance methods for 'SuperDoitCommand'
+
+category: '*superdoit-core35x'
+method: SuperDoitCommand
+compileMethod: methodSource for: aBehavior
+	aBehavior compileMethod: methodSource
+%
 
 category: '*superdoit-core31-5'
 method: SuperDoitCommand

gemstone/gemstoneVersionMap.ston
@@ -1,4 +1,10 @@
 {
+	'3.4.0' : 'bootstrapSuperDoit_34x',
+	'3.4.1' : 'bootstrapSuperDoit_34x',
+	'3.4.2' : 'bootstrapSuperDoit_34x',
+	'3.4.3' : 'bootstrapSuperDoit_34x',
+	'3.4.4' : 'bootstrapSuperDoit_34x',
+	'3.4.5' : 'bootstrapSuperDoit_34x',
 	'3.5.0' : 'bootstrapSuperDoit_35x',
 	'3.5.1' : 'bootstrapSuperDoit_35x',
 	'3.5.2' : 'bootstrapSuperDoit_35x',

rowan/components/SuperDoit.ston
@@ -3,7 +3,9 @@ RwSimpleProjectLoadComponentV2 {
 	#condition : 'common',
 	#projectNames : [ ],
 	#componentNames : [
+		'gemstone31-4/SuperDoit',
 		'gemstone31-5/SuperDoit',
+		'gemstone35/SuperDoit',
 		'gemstone36-/SuperDoit',
 		'solo/SuperDoit',
 		'stone/SuperDoit',

rowan/components/gemstone31-4/SuperDoit.ston
@@ -1,0 +1,10 @@
+RwPlatformNestedProjectLoadComponentV2 {
+	#name : 'gemstone31-4/SuperDoit',
+	#condition : [
+		'gs3.[1-4]'
+	],
+	#packageNames : [
+		'SuperDoit-Core31-4'
+	],
+	#comment : 'Mods used by 3.1.x through 3.4.x'
+}

rowan/components/gemstone35/SuperDoit.ston
@@ -1,0 +1,10 @@
+RwPlatformNestedProjectLoadComponentV2 {
+	#name : 'gemstone35/SuperDoit',
+	#condition : [
+		'gs3.5x'
+	],
+	#packageNames : [
+		'SuperDoit-Core35x'
+	],
+	#comment : 'Mods used 3.5.x'
+}

commit 33c1b006c0b8b0aee415b3f70d2b8c5670b97872

Issue #10: fix 3.6.1 regression in previous commit [6b3f594d0]

bin/generateBootstrapGsFiles.solo
@@ -63,8 +63,10 @@ generateVersionMap
 		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_34x' ].
 	#('3.5.0' '3.5.1' '3.5.2' '3.5.3' '3.5.4' '3.5.5' '3.5.6') 
 		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_35x' ].
-	#('3.6.0' '3.6.1') 
-		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_36x' ].
+	#('3.6.0') 
+		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_360' ].
+	#('3.6.1') 
+		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_361x' ].
 	#('3.7.0') 
 		do: [:gsVersion | dict at: gsVersion put: 'bootstrapSuperDoit_37x' ].
 	orderedDict := GsTonelOrderedDictionary new.

bin/superdoit_stone
@@ -98,7 +98,8 @@ if [ "$useBootstrapSuperDoit_file" = "true" ]; then
 	case $vers in
 		3.1.0.6|3.4.0) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_34x.gs" ;;
 		3.5.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_35x.gs" ;;
-		3.6.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_36x.gs" ;;
+		3.6.0) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_360.gs" ;;
+		3.6.1) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_361x.gs" ;;
 		3.7.*) export bootstrapSuperDoit_file="$gemstoneDir/bootstrap/bootstrapSuperDoit_37x.gs" ;;
 		*) echo "unsupported gemstone version $vers"; exit 1 ;;
 	esac

gemstone/bootstrap/bootstrapSuperDoit_34x.gs
@@ -1,5 +1,5 @@
 ! superDoit fileout
-!	2021-08-09T12:34:23.284653-07:00
+!	2021-08-09T13:06:44.462717-07:00
 
 ! Class Declarations
 ! Generated file, do not Edit

gemstone/bootstrap/bootstrapSuperDoit_35x.gs
@@ -1,5 +1,5 @@
 ! superDoit fileout
-!	2021-08-09T12:34:23.208774-07:00
+!	2021-08-09T13:06:44.400164-07:00
 
 ! Class Declarations
 ! Generated file, do not Edit

gemstone/bootstrap/bootstrapSuperDoit_360.gs
@@ -1,5 +1,5 @@
 ! superDoit fileout
-!	2021-08-09T13:06:44.522671-07:00
+!	2021-08-09T13:06:44.328283-07:00
 
 ! Class Declarations
 ! Generated file, do not Edit

gemstone/bootstrap/bootstrapSuperDoit_361x.gs
@@ -1,5 +1,5 @@
 ! superDoit fileout
-!	2021-08-09T12:34:23.127383-07:00
+!	2021-08-09T13:06:44.252733-07:00
 
 ! Class Declarations
 ! Generated file, do not Edit
@@ -1601,6 +1601,16 @@ _specsDict
 			option shortName
 				ifNotNil: [ :shortName | (specsDict at: 'short') at: shortName put: option ] ].
 	^ specsDict
+%
+
+! Class extensions for 'GsFile'
+
+!		Instance methods for 'GsFile'
+
+category: '*superdoit-gemstone-kernel36x'
+method: GsFile
+print: anObject
+ anObject printOn: self
 %
 
 ! Class extensions for 'SuperDoitCommand'

gemstone/gemstoneVersionMap.ston
@@ -12,7 +12,7 @@
 	'3.5.4' : 'bootstrapSuperDoit_35x',
 	'3.5.5' : 'bootstrapSuperDoit_35x',
 	'3.5.6' : 'bootstrapSuperDoit_35x',
-	'3.6.0' : 'bootstrapSuperDoit_36x',
-	'3.6.1' : 'bootstrapSuperDoit_36x',
+	'3.6.0' : 'bootstrapSuperDoit_360',
+	'3.6.1' : 'bootstrapSuperDoit_361x',
 	'3.7.0' : 'bootstrapSuperDoit_37x'
 }

rowan/components/stone/SuperDoit.ston
@@ -2,7 +2,9 @@ RwSimpleNestedProjectLoadComponentV2 {
 	#name : 'stone/SuperDoit',
 	#condition : 'stone',
 	#projectNames : [ ],
-	#componentNames : [ ],
+	#componentNames : [ 
+		'stone/gemstone361-/SuperDoit'
+	],
 	#packageNames : [
 		'SuperDoit-Stone-Core'
 	],

rowan/components/stone/gemstone361-/SuperDoit.ston
@@ -1,0 +1,21 @@
+RwPlatformNestedProjectLoadComponentV2 {
+	#name : 'stone/gemstone36-/SuperDoit',
+	#condition : [
+		'gs3.6.[1-]'
+	],
+	#packageNames : [
+		'SuperDoit-GemStone-Kernel36x'
+	],
+	#conditionalPackageMapSpecs : {
+		'gemstone' : {
+			'allusers' : {
+				#packageNameToPlatformPropertiesMap : {
+					'SuperDoit-GemStone-Kernel36x' : {
+						'symbolDictName' : 'Globals'
+					}
+				}
+			}
+		}
+	},
+	#comment : 'GsFile>>print: is missing from 3.6.x, needed for 3.6.1 and beyond because STON is present in the image'
+}