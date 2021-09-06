e738202e096b5fde7d1c775e43d0a6565a9014d7
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2021-09-06 10:38:05 -0700

    Issue #19: GEMSTONE must be set in .yml

--- dev/downloadGemStone.sh
+++ dev/downloadGemStone.sh
@@ -12,11 +12,9 @@ if [ "$PLATFORM" = "macos-10.15" ] ; then
   attach_path=`echo $attach_result | xpath "//dict/array/dict[true]/key[.='mount-point']/following-sibling::string[1]/text()" 2>/dev/null`
   cp -R "${attach_path}/${gsvers}/${name}" .
  	hdiutil detach ${attach_device}
-	export GEMSTONE="`pwd`/$name"
 else
   name=GemStone64Bit${vers}-x86_64.Linux
   zipfile=${name}.zip
   curl -O -s -S https://downloads.gemtalksystems.com/pub/GemStone64/${vers}/$zipfile
   unzip -q $zipfile
-	export GEMSTONE="`pwd`/$name"
 fi 
