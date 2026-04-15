b9a2794e7d77d7d46e98c9c0dd8fc5ed27765cdb
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2026-04-15 10:35:55 -0700

    Issue #76: debugging

--- bin/install.sh
+++ bin/install.sh
@@ -13,7 +13,7 @@ else
 			# we're good
 			;;
 		*)
-			echo "only gemstone version 3.7.0, 3.7.1 or 3.7.4.3 or 3.7.5 should be used"
+			echo "only gemstone version 3.7.0, 3.7.1, 3.7.4.3 or 3.7.5 should be used"
 			exit 1
 	esac
 fi
